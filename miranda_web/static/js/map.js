3162cfc4da203a1523c264f8482fbc52b07afc32
Mary Levchenko <wildmary@yandex.ru>
2021-12-21 05:14:05 +0300

    Add files via upload

---
+++ miranda_web/static/js/map.js
@@ -0,0 +1,227 @@
+var data = [
+  {
+    "lng": -65.2,
+    "lat": 19.12,
+    "text": "<b>Data:</b> 17th Nov 1807 <br> <b>Day of week:</b> Tuesday <br> <b>Weather:</b> Pleasant Breezes and fair weather, but rough Sea. <br> <b>Text:</b> The Netley and Convoy in company."
+  },
+  {
+    "lng": -64.48,
+    "lat": 19.5,
+    "text": "<b>Data:</b> 18th Nov 1807 <br> <b>Day of week:</b> Wednesday <br> <b>Weather:</b> Light Winds and fine weather."
+  },
+  {
+    "lng": -64.47,
+    "lat": 20.15,
+    "text": "<b>Data:</b> 19th Nov 1807 <br> <b>Day of week:</b> Thursday <br> <b>Weather:</b> Fresh Breezes and cloudy weather, accompanied with rain."
+  },
+  {
+    "lng": -65,
+    "lat": 20.58,
+    "text": "<b>Data:</b> 20th Nov 1807 <br> <b>Day of week:</b> Friday <br> <b>Weather:</b> Light Winds and fair weather."
+  },
+  {
+    "lng": -65.4,
+    "lat": 21.57,
+    "text": "<b>Data:</b> 21th Nov 1807 <br> <b>Day of week:</b> Saturday <br> <b>Weather:</b> Light airs and fine weather. <br> <b>Text:</b> The Netleys signal was made to speak the Commodore, about 5 P.M. Lieutt. [space left] came on board, when Captn: Cochrane delivered him sundry Letters and Dispatches, with orders to return to Tortola. I wrote Fitzwilliam by this Opportunity."
+  },
+  {
+    "lng": -65.11,
+    "lat": 22.35,
+    "text": "<b>Data:</b> 22th Nov 1807 <br> <b>Day of week:</b> Sunday <br> <b>Weather:</b> Light Winds and fair weather."
+  },
+  {
+    "lng": -65.11,
+    "lat": 23.07,
+    "text": "<b>Data:</b> 23th Nov 1807 <br> <b>Day of week:</b> Monday <br> <b>Weather:</b> Light Winds and pleasant weather. <br> <b>Text:</b> 2 Vessels of the Convoy missing. Saw a strange Sail."
+  },
+  {
+    "lng": -65.28,
+    "lat": 23.18,
+    "text": "<b>Data:</b> 24th Nov 1807 <br> <b>Day of week:</b> Tuesday <br> <b>Weather:</b> Calm"
+  },
+  {
+    "lng": -65.33,
+    "lat": 23.28,
+    "text": "<b>Data:</b> 25th Nov 1807 <br> <b>Day of week:</b> Wednesday <br> <b>Weather:</b> Pleasant Breezes and fair weather. <br> <b>Text:</b> Saw a strange sail to Windward."
+  },
+  {
+    "lng": -65.55,
+    "lat": 24.19,
+    "text": "<b>Data:</b> 26th Nov 1807 <br> <b>Day of week:</b> Thursday <br> <b>Weather:</b> Pleasant Breezes with fine weather. <br> <b>Text:</b> 2 more of the Convoy missing, one of which was the British Queen."
+  },
+  {
+    "lng": -65.39,
+    "lat": 25.29,
+    "text": "<b>Data:</b> 27th Nov 1807 <br> <b>Day of week:</b> Friday <br> <b>Weather:</b> Pleasant Breezes and fair weather. <br> <b>Text:</b> The Sloop Mary, bound to Newfoundland, parted company by permission from the Commodore."
+  },
+  {
+    "lng": -65.43,
+    "lat": 26.24,
+    "text": "<b>Data:</b> 28th Nov 1807 <br> <b>Day of week:</b> Saturday <br> <b>Weather:</b> Light winds and cloudy weather. <br> <b>Text:</b> One of the sick died, and was committed to the deep about Sun set."
+  },
+  {
+    "lng": -64.53,
+    "lat": 26.15,
+    "text": "<b>Data:</b> 29th Nov 1807 <br> <b>Day of week:</b> Sunday <br> <b>Weather:</b> Light winds inclining to calm, with fine weather in the early part of this day, the latter part strong Breezes and heavy Sea."
+  },
+  {
+    "lng": -63.55,
+    "lat": 26.16,
+    "text": "<b>Data:</b> 30th Nov 1807 <br> <b>Day of week:</b> Monday <br> <b>Weather:</b> Fresh Breezes with rough Sea, and fair weather."
+  },
+  {
+    "lng": -63.15,
+    "lat": 26.02,
+    "text": "<b>Data:</b> 1st Dec 1807 <br> <b>Day of week:</b> Tuesday"
+  },
+  {
+    "lng": -63.59,
+    "lat": 26.47,
+    "text": "<b>Data:</b> 2d Dec 1807 <br> <b>Day of week:</b> Wednesday <br> <b>Weather:</b> Light winds and fine weather. <br> <b>Text:</b> One more of the Convoy missing."
+  },
+  {
+    "lng": -64.24,
+    "lat": 27.5,
+    "text": "<b>Data:</b> 3d Dec 1807 <br> <b>Day of week:</b> Sunday <br> <b>Weather:</b> Light airs with the fair weather. <br> <b>Text:</b> This day the ship British Queen rejoined the Convoy."
+  },
+  {
+    "lng": -64.7,
+    "lat": 28.03,
+    "text": "<b>Data:</b> 4th Dec 1807 <br> <b>Day of week:</b> Friday <br> <b>Weather:</b> Light Breezes and fair weather."
+  },
+  {
+    "lng": -63.56,
+    "lat": 29.04,
+    "text": "<b>Data:</b> 5th Dec 1807 <br> <b>Day of week:</b> Saturday <br> <b>Weather:</b> Fine fair Winds and weather."
+  },
+  {
+    "lng": -61.19,
+    "lat": 30.32,
+    "text": "<b>Data:</b> 6th Dec 1807 <br> <b>Day of week:</b> Sunday <br> <b>Weather:</b> Fair Wind and weather. The latter part strong Breezes from the N.W. with heavy Sea."
+  },
+  {
+    "lng": -59.28,
+    "lat": 32,
+    "text": "<b>Data:</b> 7th Dec 1807 <br> <b>Day of week:</b> Monday <br> <b>Weather:</b> Strong Breezes and squally weather, with a rough Sea. <br> <b>Text:</b> Do. obsd. 60°.37. <br> Spoke the Ship Pomona, 8 days from New York bound to Guadeloupe, who informed us that the dispute between England and America was amicably adjusted."
+  },
+  {
+    "lng": -57.42,
+    "lat": 33.4,
+    "text": "<b>Data:</b> 8th Dec 1807 <br> <b>Day of week:</b> Tuesday <br> <b>Weather:</b> Moderate Breezes and fair weather. <br> <b>Text:</b> Obsd. 58°.51. <br> One of the sick died. Saw a strange Vessel bound to the Southward. <br> One more of the Convoy missing."
+  },
+  {
+    "lng": -57.29,
+    "lat": 33.3,
+    "text": "<b>Data:</b> 9th Dec 1807 <br> <b>Day of week:</b> Wednesday <br> <b>Weather:</b> Light Breezes and fine Weather. <br> <b>Text:</b> Saw a strange sail steering to the Southward. <br> Two more of the Convoy missing."
+  },
+  {
+    "lng": -55.36,
+    "lat": 33.28,
+    "text": "<b>Data:</b> 10th Dec 1807 <br> <b>Day of week:</b> Thursday <br> <b>Weather:</b> The early part of this day light Breezes inclining to calm, the latter part fine Breezes from S.W. <br> <b>Text:</b> Obsd. 56°.45. <br> At 10 A.M. Captn. of the Ship Otter came on board and informed the Commodore his ship had sprung a leak, and requested the Carpenter might be sent on board to examine it, this was complyed [sic!] with, and the Carpenter on his return reported to Captn. Cochrane, that the smallest Gale would endanger the lives of the People, as she must inevitably founder, there being no possibility of stopping the leak. Mr: Phillips /the Master/ and Lieutt. White, accompanied by the Carpenter, went together on board, and after due examination confirmed the above report. A Signal was made for all Boats, and they were ordered to attend the Otter, to save such articles as could conveniently be come at, previous to her being destroyed. Captn. Cochrane acoompanied by Captn. Campbell attended this operation, and sent from on board the Otter sundry Packages of Indigo, Cotton, and Elephants Teeth, with part of her Crew. At 5 P.M. she was set on fire, and afforded us a spectacle awfully grand."
+  },
+  {
+    "lng": -54.29,
+    "lat": 34.43,
+    "text": "<b>Data:</b> 11th Dec 1807 <br> <b>Day of week:</b> Friday <br> <b>Weather:</b> Fresh Breezes and fair weather. <br> <b>Text:</b> Obsd. 55°.38. <br> The latter part of this day it blew a Gale from the S.W. with a heavy Sea: we scud under bare Poles, in order to keep company with the Convoy."
+  },
+  {
+    "lng": -52.49,
+    "lat": 36.35,
+    "text": "<b>Data:</b> 12th Dec 1807 <br> <b>Day of week:</b> Saturday <br> <b>Weather:</b> Strong Breezes with fair weather. <br><b>Text:</b> Obsd. 53°.58. <br> One of the Convoy missing. Saw a strange Sail."
+  },
+  {
+    "lng": -50.54,
+    "lat": 37.41,
+    "text": "<b>Data:</b> 13th Dec 1807 <br> <b>Day of week:</b> Sunday <br> <b>Weather:</b> Moderate Breezes and fine weather. <br> <b>Text:</b> Two more of the Convoy missing."
+  },
+  {
+    "lng": -48.54,
+    "lat": 39.5,
+    "text": "<b>Data:</b> 14th Dec 1807 <br> <b>Day of week:</b> Monday <br> <b>Weather:</b> Fresh Breezes and fair weather. <br> <b>Text:</b> Two more of the Convoy missing."
+  },
+  {
+    "lng": -46.25,
+    "lat": 41.7,
+    "text": "<b>Data:</b> 15th Dec 1807 <br> <b>Day of week:</b> Tuesday <br> <b>Weather:</b> Strong Breezes and fine weather, but rough Sea and cloudy atmosphere."
+  },
+  {
+    "lng": -42.11,
+    "lat": 43.16,
+    "text": "<b>Data:</b> 16th Dec 1807 <br> <b>Day of week:</b> Wednesday <br> <b>Weather:</b> This day it blew a Gale from N.W. with frequent showers of Hail and Rain, and very high Sea. <br> <b>Text:</b> Nine of the Convoy only in sight, one of which had a signal of distress flying, and was answered by the Commodore, but we could neither speak him, or afford him any relief on account of the very great Sea, which would not admit the Boats to be hoisted out."
+  },
+  {
+    "lng": -42.58,
+    "lat": 40.18,
+    "text": "<b>Data:</b> 17th Dec 1807 <br> <b>Day of week:</b> Thursday <br> <b>Weather:</b> Continuation of the Gale with the Wind from the S.W. and rain. <br> <b>Text:</b> Scudding under bare Poles. At Noon none of the Convoy in sight. One of the sick Men died."
+  },
+  {
+    "lng": -37.2,
+    "lat": 43.3,
+    "text": "<b>Data:</b> 18th Dec 1807 <br> <b>Day of week:</b> Friday <br> <b>Weather:</b> This day the Gale encreased, with the Wind from the Northward, accompanied with frequent showers of Hail, and very high Sea. <br> <b>Text:</b> The Ship lying to under a trysail."
+  },
+  {
+    "lng": -36.38,
+    "lat": 43.12,
+    "text": "<b>Data:</b> 19th Dec 1807 <br> <b>Day of week:</b> Saturday <br> <b>Weather:</b> Moderate Breezes and cloudy weather with rough Sea. Saw a Sail to Nd:. <br> <b>Text:</b> A sick Man died."
+  },
+  {
+    "lng": -33.19,
+    "lat": 44.15,
+    "text": "<b>Data:</b> 20th Dec 1807 <br> <b>Day of week:</b> Sunday <br> <b>Weather:</b> Moderate Breezes with Rain and Fogs."
+  },
+  {
+    "lng": -30.28,
+    "lat": 46.49,
+    "text": "<b>Data:</b> 21th Dec 1807 <br> <b>Day of week:</b> Monday <br> <b>Weather:</b> Strong Breezes and cloudy weather."
+  },
+  {
+    "lng": -31.1,
+    "lat": 48.4,
+    "text": "<b>Data:</b> 22th Dec 1807 <br> <b>Day of week:</b> Tuesday <br> <b>Weather:</b> Pleasant Breezes with remarkable fine weather."
+  },
+  {
+    "lng": -26.49,
+    "lat": 49.2,
+    "text": "<b>Data:</b> 23th Dec 1807 <br> <b>Day of week:</b> Wednesday <br> <b>Weather:</b> Fresh Breezes and fair weather."
+  },
+  {
+    "lng": -19.02,
+    "lat": 49.2,
+    "text": "<b>Data:</b> 24th Dec 1807 <br> <b>Day of week:</b> Thursday <br> <b>Weather:</b> Fresh Breezes with rain and hazy weather. <br> <b>Text:</b> One of the sick died."
+  },
+  {
+    "lng": -14.37,
+    "lat": 49.1,
+    "text": "<b>Data:</b> 25th Dec 1807 <br> <b>Day of week:</b> Friday <br> <b>Weather:</b> Pleasant Breezes and variable weather. <br> <b>Text:</b> Another of the sick died."
+  },
+  {
+    "lng": -11.34,
+    "lat": 49.32,
+    "text": "<b>Data:</b> 26th Dec 1807 <br> <b>Day of week:</b> Saturday <br> <b>Weather:</b> Fresh Breezes and fair weather. <br> <b>Text:</b> Sounded but got no bottom."
+  },
+  {
+    "lng": -9.2,
+    "lat": 49.2,
+    "text": "<b>Data:</b> 27th Dec 1807 <br> <b>Day of week:</b> Sunday <br> <b>Weather:</b> Strong Breezes and fair weather. <br> <b>Text:</b> At 8 A.M. spoke Transport № 3 from Falmouth bound to [space left] who informd us that Scilly bore E. by N.W. 4 Miles distant. <br> 3 other Vesseld in sight. At 4 P.M. got soundings in 80 Fathoms. Lying to part of the Night, and frequently sounding."
+  },
+  {
+    "lng": -5.46,
+    "lat": 49.26,
+    "text": "<b>Data:</b> 28th Dec 1807 <br> <b>Day of week:</b> Monday <br> <b>Weather:</b> This day it blew a Gale from the S.W. with heavy Sea, Rain and hazy weather. <br> Lying to part of the Night, and frequently sounding."
+  }
+]
+
+var map = L.map('map').setView([43.12, -36.38], 3).addControl(new L.Control.Fullscreen());
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+}).addTo(map);
+
+data.forEach((point) => {
+	var marker = L.circle([point.lat, point.lng], {
+		color: 'steelblue',
+	    opcaity: 0,
+	    fillOpacity: 0.8,
+	    radius: 35000
+	}).addTo(map);
+	marker.bindPopup(point.text);
+})
